6cf9bc327fda65a296bbb88d963c64ba4720fed0
Dark <lwz22psp@hotmail.com>
2019-03-21 21:25:00 +0800

    bug fix
    Signed-off-by: Dark <lwz22psp@hotmail.com>

--- src/main/java/com/hapiniu/demo/springbootdocker/controller/HelloWorldController.java
+++ src/main/java/com/hapiniu/demo/springbootdocker/controller/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.hapiniu.demo.springbootdocker.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "<h1>Hello Spring-Boot Maven Docker</h1>";
+    }
 }
