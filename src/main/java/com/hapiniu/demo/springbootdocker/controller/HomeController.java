4d561bd4c80aa7221db5006d217616facb389e3a
Dark <lwz22psp@hotmail.com>
2019-03-22 09:31:37 +0800

    home controller
    Signed-off-by: Dark <lwz22psp@hotmail.com>

---
+++ src/main/java/com/hapiniu/demo/springbootdocker/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.hapiniu.demo.springbootdocker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String homeMessage() {
+        return "<h1>老西门瞎唔搞系统</h1><div>by hapiniu</div>";
+    }
+}
