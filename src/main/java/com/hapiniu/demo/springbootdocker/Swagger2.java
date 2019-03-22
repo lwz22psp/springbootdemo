d2cca82d248169ba946783973eecbfb4fbf06da5
Dark <lwz22psp@hotmail.com>
2019-03-22 14:50:03 +0800

    add swagger
    Signed-off-by: Dark <lwz22psp@hotmail.com>

--- src/main/java/com/hapiniu/demo/springbootdocker/Swagger2.java
+++ src/main/java/com/hapiniu/demo/springbootdocker/Swagger2.java
@@ -1,4 +1,36 @@
 package com.hapiniu.demo.springbootdocker;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hapiniu.demo.springbootdocker"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("老西门应用  APIs")
+                .description("--------------------------------")
+                .termsOfServiceUrl("https://github.com/lwz22psp/springbootdemo")
+                .contact(new Contact("hapiniu","https://github.com/lwz22psp","lwz22psp@hotmail.com"))
+                .version("0.1.1")
+                .build();
+    }
 }
