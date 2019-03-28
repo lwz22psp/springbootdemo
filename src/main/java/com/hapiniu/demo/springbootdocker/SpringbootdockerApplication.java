d4c66045664ab3209cfe5c59a7fe2467e3dc8e2b
Dark <lwz22psp@hotmail.com>
2019-03-28 14:28:15 +0800

    eureka client regest
    Signed-off-by: Dark <lwz22psp@hotmail.com>

--- src/main/java/com/hapiniu/demo/springbootdocker/SpringbootdockerApplication.java
+++ src/main/java/com/hapiniu/demo/springbootdocker/SpringbootdockerApplication.java
@@ -3,10 +3,11 @@ package com.hapiniu.demo.springbootdocker;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan(value = {"com.hapiniu.demo.springbootdocker"})
+@EnableEurekaClient
 public class SpringbootdockerApplication {
 
     public static void main(String[] args) {
