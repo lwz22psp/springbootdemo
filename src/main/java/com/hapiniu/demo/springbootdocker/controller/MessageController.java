8ac14b23bb22ed8f3f5803f0c4c9f37affe05bd8
Dark <lwz22psp@hotmail.com>
2019-05-13 15:29:47 +0800

    t
    Signed-off-by: Dark <lwz22psp@hotmail.com>

--- src/main/java/com/hapiniu/demo/springbootdocker/controller/MessageController.java
+++ src/main/java/com/hapiniu/demo/springbootdocker/controller/MessageController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
 /**
  * @author dark
  **/
@@ -31,11 +36,30 @@ public class MessageController {
             resp.setFromUserName(request.getToUserName());
             resp.setToUserName(request.getFromUserName());
             resp.setCreateTime(request.getCreateTime());
-            return JSON.toJSONString(resp);
-
+            return jaxbObjectToXML(resp);
         } catch (Exception e) {
             return "success";
         }
 
     }
+
+    private static String jaxbObjectToXML(WechatMessageResponse customer) {
+        String xmlString = "";
+        try {
+            JAXBContext context = JAXBContext.newInstance(WechatMessageResponse.class);
+            Marshaller m = context.createMarshaller();
+
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE); // To format XML
+
+            StringWriter sw = new StringWriter();
+            m.marshal(customer, sw);
+            xmlString = sw.toString();
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        return xmlString;
+    }
+
 }
